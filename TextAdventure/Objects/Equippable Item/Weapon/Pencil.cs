a10f58c816dfe9963fec487d66bd61be888b74b5
Tim <frobergtd@gmail.com>
2016-10-16 20:12:10 -0400

    Worked on the weapons, added an explosion class. Preparing for containers.

---
+++ TextAdventure/Objects/Equippable Item/Weapon/Pencil.cs
@@ -0,0 +1,32 @@
+﻿using TextAdventure.Living_Critter;
+
+namespace TextAdventure.Objects.Equippable_Item.Weapon
+{
+    class Pencil
+    {
+        int damage;
+        int brutishnessChanged;
+
+        /// <summary>Constructor for a sword</summary>
+        /// <param name="s">The type of sword</param>
+        /// <param name="h">Human character to modify</param>
+        public Pencil(Human h)
+        {
+            damage = 2;
+            brutishnessChanged = 15;
+            addDamage(h);
+        }
+
+        /// <summary>Adds that amount of damage to the character</summary>
+        public void addDamage(Human h)
+        {
+            h.SetAddedDamage(damage);
+        }
+
+        /// <summary>Changes the specified stats by the specified amount</summary>
+        public void adjustStats(Human h)
+        {
+            h.SetBrutishness(brutishnessChanged);
+        }
+    }
+}
---
+++ TextAdventure/Objects/Explosion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TextAdventure.Living_Critter;
+
+namespace TextAdventure.Objects
+{
+    class Explosion
+    {
+        public Explosion(Human h, string s)
+        {
+            int ceiling = h.Hitpoints/2;
+            int damage = explosionDamage(s, ceiling);
+        }
+
+        protected int explosionDamage(string s, int ceiling)
+        {
+            var rand = new Random();
+            int temp = rand.Next(0, ceiling+1);
+            return 0;
+        }
+    }
+}
